a6118e3897fa2a3d9feb05d2b710cbd4252cff90
RyhaveanProject <bfnsscxj97@privaterelay.appleid.com>
2026-05-13 03:22:18 +0400

    Update __init__.py

--- app/handlers/__init__.py
+++ app/handlers/__init__.py
@@ -1,1 +1,11 @@
+"""Aggregates handler registration."""
+from pyrogram import Client
 
+from app.audio.vc_manager import VCManager
+from app.handlers import commands, chat, voice_chat
+
+
+def register_all_handlers(client: Client, vc: VCManager) -> None:
+    commands.register(client, vc)
+    voice_chat.register(client, vc)
+    chat.register(client, vc)
